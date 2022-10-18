8aeb6e4f9730c184b70de6426c28c369f6141b34
Pedro Henrique Garcia <pehdfms@gmail.com>
2022-10-18 19:48:11 -0300

    refactor: abstract operations on random numbers

--- refactor.js
+++ refactor.js
@@ -2,26 +2,16 @@ function returnRandomNumber() {
   return Math.random();
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
+function operateRandomNumber(num, operation) {
+    const number = returnRandomNumber();
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+    return `Seu número é ${operation(Math.round(number * 100), num)}!`;
 }
 
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
-}
+// Para não estragarmos a interface publica, recriamos os metodos antigos com a nova mudança
+// Além de diminuir a repetição de código, agora podemos também criar operações arbitrarias
+// utilizando operateRandomNumber.
+const sumToRandomNumber = (num) => operateRandomNumber(num, (a, b) => a + b);
+const subtractRandomNumber = (num) => operateRandomNumber(num, (a, b) => a - b);
+const multiplyToRandomNumber = (num) => operateRandomNumber(num, (a, b) => a * b);
+const divideRandomNumber = (num) => operateRandomNumber(num, (a, b) => a / b);
