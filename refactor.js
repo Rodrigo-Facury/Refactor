36a414344b9a621125b4377f4096b4c14c7504c4
Brayan Wilis <BrayanWilis@outlook.com>
2022-10-18 20:16:15 -0300

    [ Refact ]

--- refactor.js
+++ refactor.js
@@ -1,27 +1,22 @@
-function returnRandomNumber() {
-  return Math.random();
+function processNumberByRandom(num, process) {
+  const randNum = Math.round(Math.random() * 100);
+  switch (process) {
+    case "sum":
+      return `Seu número é ${randNum + num}!`;
+      break;
+    case "divide":
+      return `Seu número é ${randNum / num}!`;
+      break;
+    case "multiply":
+      return `Seu número é ${randNum * num}!`;
+      break;
+    case "subtract":
+      return `Seu número é ${randNum - num}!`;
+      break;
+    default:
+      return `Seu número é ${randNum + num}!`;
+      break;
+  }
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
-}
-
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
-}
+console.log(processNumberByRandom(12, "divide"));
