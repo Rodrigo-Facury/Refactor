955e68f9cc63dabe6bc39c9af06785388bab36e2
DucilioManjate <liomanjate@gmail.com>
2022-10-18 22:50:18 -0300

    refact

--- refactor.js
+++ refactor.js
@@ -1,27 +1,38 @@
-function returnRandomNumber() {
+export function returnRandomNumber() {
   return Math.random();
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
+export function returnRandomNumber(num, operation) {
+  const operations = {
+    numberToSum: (num) => num + returnRandomNumber(),
+    numberToSubtract: (num) => num - returnRandomNumber(),
+    numberToMultiply: (num) => num * returnRandomNumber(),
+    numberToDivide: (num) => num / returnRandomNumber(),
+  };
+  return `Seu número é ${Math.round(operations[operation](num) * 100)}!`;
 
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
 }
 
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
+// function sumToRandomNumber(num) {
+//   const numberToSum = returnRandomNumber();
 
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
+//   return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
+// }
 
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
+// function subtractRandomNumber(num) {
+//   const numberToSubtract = returnRandomNumber();
 
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
+//   return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
+// }
 
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
+// function multiplyToRandomNumber(num) {
+//   const numberToMultiply = returnRandomNumber();
 
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
-}
+//   return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
+// }
+
+// function divideRandomNumber(num) {
+//   const numberToDivide = returnRandomNumber();
+
+//   return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+// }
