f067c4d5bbb2e882b54863a15a050f26f9a1d262
AnaFranca <50755335+AnaFranca@users.noreply.github.com>
2022-11-27 10:44:30 -0300

    Update refactor.js
    Finalizado

--- refactor.js
+++ refactor.js
@@ -1,27 +1,15 @@
-function returnRandomNumber() {
-  return Math.random();
+function RandomNumber () {
+  const returnNumber = (number) => `Seu número é ${number}!`
+
+  this.randomNumber = Math.random()
+  const roundedRandomNumber = Math.round(this.randomNumber * 100)
+
+  this.sum = (number) => returnNumber(roundedRandomNumber + number)
+  this.subtract = (number) => returnNumber(roundedRandomNumber - number)
+  this.multiply = (number) => returnNumber(roundedRandomNumber * number)
+  this.divide = (number) => returnNumber(roundedRandomNumber / number)
 }
 
-function sumToRandomNumber(num) {
-  const numberToSum = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSum * 100) + num}!`;
-}
-
-function subtractRandomNumber(num) {
-  const numberToSubtract = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToSubtract * 100) - num}!`;
-}
-
-function multiplyToRandomNumber(num) {
-  const numberToMultiply = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToMultiply * 100) * num}!`;
-}
-
-function divideRandomNumber(num) {
-  const numberToDivide = returnRandomNumber();
-
-  return `Seu número é ${Math.round(numberToDivide * 100) / num}!`;
+RandomNumber.prototype.toString = function () {
+  return this.randomNumber
 }
